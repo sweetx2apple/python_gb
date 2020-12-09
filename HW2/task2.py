d7943969d97497fd56a4f3b5230e3e811fcae5be
sweetx2apple <metrshyisyat@yahoo.co.jp>
2020-12-09 18:06:36 +0400

    абракадабра

---
+++ HW2/task2.py
@@ -0,0 +1,24 @@
+#Для списка реализовать обмен значений соседних элементов,
+# т.е. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+# При нечетном количестве элементов последний сохранить на своем месте.
+# Для заполнения списка элементов необходимо использовать функцию input().
+
+list = input('Введите элементы списка: ')
+my_list = []
+iiiiiii
+# sudo a2dismod tupizm
+# sudo a2enmod mozg
+for i in range(1,len(list), 2):
+    my_list.append(i-1)
+
+print(my_list)
+
+#AAAAaaaaaaaaaaaaaa
+
+#my_list.append(list)
+
+#    my_list[x], my_list[x + 1] = my_list[x + 1], my_list[x]
+#    x += 2
+#asdjfkl;
+
+
